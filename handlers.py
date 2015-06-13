7c9e836b2e05d4d220e6aa3ce28e9872f409c672
nare469 <nare469@gmail.com>
2015-06-13 19:55:01 -0400

    Tabs to spaces

--- handlers.py
+++ handlers.py
@@ -1,36 +1,45 @@
 # Main file used for the endpoints
 import tornado.web
+import const
+from pymongo import MongoClient
+
+client = MongoClient(const.MONGO_URL)
+db = client['db']
+
+user_data = db['user_data']
+request_data = db['request_data']
+locations = db['locations']
 
 class BaseHandler(tornado.web.RequestHandler):
-	pass
+    pass
 
 class UserHandler(BaseHandler):
-	def get(self, uuid):
-		self.write(self.users.find_one({'uuid': uuid}))
+    def get(self, uuid):
+        self.write(user_data.find_one({'uuid': uuid}))
 
-	def post(self):
-		#Creates a new user
-		new_user = tornado.escape.json_decode(self.request.body)
-		
-		#Adds it to the database
-		self.users.insert_one(new_user)
+    def post(self):
+        #Creates a new user
+        new_user = tornado.escape.json_decode(self.request.body)
+        
+        #Adds it to the database
+        user_data.insert_one(new_user)
 
 class DataHandler(BaseHandler):
-	def post(self):
-		#Create data dict
-		data = tornado.escape.json_decode(self.request.body)
+    def post(self):
+        #Create data dict
+        data = tornado.escape.json_decode(self.request.body)
 
-		#Write to DB - Needed for ML
-		self.request_data.insert_one(data)
+        #Write to DB - Needed for ML
+        request_data.insert_one(data)
 
-		#Find user from data
-		user = self.users.find_one({'uuid': data['uuid']})
+        #Find user from data
+        user = user_data.find_one({'uuid': data['uuid']})
 
-		'''
-			The magic happens here
-			Either math or ML, whatever
-		'''
+        '''
+            The magic happens here
+            Either math or ML, whatever
+        '''
 
 class ConfirmHandler(BaseHandler):
-	def post(self):
-		correct = self.get_argument('correct')
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import tornado.ioloop
 import tornado.web
-import const
 
 from handlers import *
-from pymongo import MongoClient
 
 application = tornado.web.Application([
-	(r"/users/([a-z0-9\-]+)/?", UserHandler),
-	(r"/data/?", DataHandler),
-	(r"/confirms/?", ConfirmHandler),
+    (r"/users/?", UserHandler),
+    (r"/data/?", DataHandler),
+    (r"/confirms/?", ConfirmHandler),
 ])
 
-client = MongoClient(const.MONGO_URL)
-db = client['db']
-
 if __name__ == "__main__":
-	application.users = db['users']
-	application.user_data = db['user_data']
-	application.request_data = db['request_data']
-	application.locations = db['locations']
-	application.listen(8888)
-	tornado.ioloop.IOLoop.current().start()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
