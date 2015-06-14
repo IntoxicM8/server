fac8a09819185851a8fae6d250bc5b149b57394c
Rossem <rrsbrg@gmail.com>
2015-06-13 21:19:06 -0400

    shit started to work

--- handlers.py
+++ handlers.py
@@ -1,5 +1,6 @@
 import tornado.web
 import const
+import json
 from pymongo import MongoClient
 
 from google_api.google_places import *
@@ -12,37 +13,27 @@ class BaseHandler(tornado.web.RequestHandler):
         self.request_data = self.db['request_data']
         self.locations = self.db['locations']
 
+
 class UserHandler(BaseHandler):
     def get(self):
-        uuid = self.get_arguments('uuid')
-        user_data = json.dumps(self.user_data.find_one({'uuid': uuid}))
-        print(user_data)
-        self.write(user_data)
+        uuid = int(self.get_argument('uuid'))
+        user_data = self.user_data.find({'uuid': uuid})[0]
+        user_data.pop('_id', None)
+        self.write(json.dumps(user_data))
 
     def post(self):
-        #Creates a new user
         new_user = tornado.escape.json_decode(self.request.body)
-        
-        #Adds it to the database
         self.user_data.insert_one(new_user)
+        self.write('shit worked')
+
 
 class DataHandler(BaseHandler):
     def post(self):
-        #Create data dict
         data = tornado.escape.json_decode(self.request.body)
-
-        #Write to DB - Needed for ML
         self.request_data.insert_one(data)
 
-        #Find user from data
-        user = self.user_data.find_one({'uuid': data['uuid']})
-
-        '''
-            The magic happens here
-            Either math or ML, whatever
-        '''
-
         self.write(GooglePlaces.get_nearest_general(43.6471642, -79.38705139999999))
+
 
 class ConfirmHandler(BaseHandler):
     def post(self):
