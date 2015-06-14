24b47bedb90e2c7495fc7d1cb51e42ba51f19417
nare469 <nare469@gmail.com>
2015-06-14 02:18:19 -0400

    Made equations file better

--- equation.py
+++ equation.py
@@ -1,17 +1,40 @@
 import math
 
-AGE_CONST = {'UNDER25':    10, '25AND40': 8, '40AND60': 6, 'ABOVE60': 3}
-GENDER_CONST = {'MALE': 1, 'FEMALE': 1.2}
-TOLERANCE = {'HIGH': 0.8, 'MEDIUM': 1, 'LOW': 1.2}
-PROXIMITIES = {'CLOSETOBAR': 4, 'CLOSETOOTHER': 2, 'FAR': 1}
+GENDER_CONST = {'M': 0, 'F': 1}
+TOLERANCE = {'H': 0, 'M': 1, 'L': 2}
+PROXIMITIES = {'CLOSE': 4, 'CLOSETOOTHER': 2, 'FAR': 1}
 
-def TOD(x):
+def day_of_week(x):
+	efunc = exp(-1*pow(x - 5.3, 2)/1.5)
+	return 1+efunc
+
+def gender_func(x):
+	return x*0.2 + 1
+
+def tolerance(x):
+	return 0.8 + x*0.2
+
+def bpm_percent(x):
+	return 3/x - 2
+
+def time_of_day(x):
     if -6 < x and x < 6:
         return math.exp(-1.0*x*x/20)*1.5
     return 0.1
 
-BPM = {'FINE': 1, 'NOTFINE': 10}
+def proximity_func_bar(x):
+	return math.exp(-1.0*x*x/8192)*4
 
-DOW = {'FRIDAY': 1.5, 'SATURDAY': 1.25, 'OTHER': 1}
+def proximity_func_nightclub(x):
+	return math.exp(-1.0*x*x/8192)*4
 
-print AGE_CONST['UNDER25']*GENDER_CONST['FEMALE']*TOLERANCE['MEDIUM']*PROXIMITIES['FAR']*TOD(-0.5)*BPM['FINE']*DOW['SATURDAY']
+def proximity_func_casino(x):
+	return math.exp(-1.0*x*x/8192)*2
+
+def age_func(x):
+    if x < 12:
+        return x/17
+    elif 12 <= x and x <25
+        return pow(x-12,1.7)/8.5 + 12/17
+    else
+        return -10*x/55 + 160/11
