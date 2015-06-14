4d74888d8d78db3bd2fee618a90a015f348b9894
Rossem <rrsbrg@gmail.com>
2015-06-13 20:23:00 -0400

    app class

--- handlers.py
+++ handlers.py
@@ -1,28 +1,25 @@
-# Main file used for the endpoints
 import tornado.web
 import const
 from pymongo import MongoClient
 
-client = MongoClient(const.MONGO_URL)
-db = client['db']
-
-user_data = db['user_data']
-request_data = db['request_data']
-locations = db['locations']
-
 class BaseHandler(tornado.web.RequestHandler):
-    pass
+    def initialize(self):
+        self.db = self.application.client['db']
+        self.user_data = self.db['user_data']
+        self.request_data = self.db['request_data']
+        self.locations = self.db['locations']
 
 class UserHandler(BaseHandler):
-    def get(self, uuid):
-        self.write(user_data.find_one({'uuid': uuid}))
+    def get(self):
+        uuid = self.get_arguments(uuid)
+        self.write(self.user_data.find_one({'uuid': uuid}))
 
     def post(self):
         #Creates a new user
         new_user = tornado.escape.json_decode(self.request.body)
         
         #Adds it to the database
-        user_data.insert_one(new_user)
+        self.user_data.insert_one(new_user)
 
 class DataHandler(BaseHandler):
     def post(self):
@@ -30,10 +27,10 @@ class DataHandler(BaseHandler):
         data = tornado.escape.json_decode(self.request.body)
 
         #Write to DB - Needed for ML
-        request_data.insert_one(data)
+        self.request_data.insert_one(data)
 
         #Find user from data
-        user = user_data.find_one({'uuid': data['uuid']})
+        user = self.user_data.find_one({'uuid': data['uuid']})
 
         '''
             The magic happens here
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import tornado.ioloop
-import tornado.web
+from tornado.web import Application, url
+import tornado.httpserver
+import tornado.ioloop
+
+import logging
 
 from handlers import *
 
-application = tornado.web.Application([
-    (r"/users/?", UserHandler),
-    (r"/data/?", DataHandler),
-    (r"/confirms/?", ConfirmHandler),
-])
+
+
+
+class App(Application):
+    def __init__(self, **overrides):
+        handlers = [
+            (r"/users/?", UserHandler),
+            (r"/data/?", DataHandler),
+            (r"/confirms/?", ConfirmHandler),
+        ]
+
+        Application.__init__(self, handlers)
+        self.client = MongoClient(const.MONGO_URL)
+
 
 if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    logging.info('running shit')
+
+    application = App()
+    http_server = tornado.httpserver.HTTPServer(application)
+
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
